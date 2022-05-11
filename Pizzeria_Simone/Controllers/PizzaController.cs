10cba0586e5864bab940116261a34129a078af58
Simone Serpi <simoneserpi@hotmail.it>
2022-05-11 08:54:50 +0200

    Ho provato ad effettuare le modifiche capendoci poco

---
+++ Pizzeria_Simone/Controllers/PizzaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pizzeria_Simone.Models;
+using Pizzeria_Simone.Utils;
+
+namespace Pizzeria_Simone.Controllers
+{
+    
+    public class PizzaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Pizza> pizza = PizzaData.GetPizza();
+            return View("HomePage", pizza);
+        }
+    }
+}
---
+++ Pizzeria_Simone/Models/Pizza.cs
@@ -0,0 +1,27 @@
+﻿namespace Pizzeria_Simone.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public float Price { get; set; }
+
+
+        public Pizza()
+        {
+
+        }
+
+        public Pizza(int id, string title, string Description, string image, float Price)
+        {
+            this.Id = Id;
+            this.Title = Title;
+            this.Description = Description; 
+            this.Image = Image;
+            this.Price = Price;
+
+        }
+    }
+}
